7af035c06087f7b25e22fba5992df16f8c834341
LartyHD <LartyHD@minewar.de>
2018-05-05 20:16:31 +0200

    Delete

--- src/main/kotlin/de/lartyhd/spigot/classic/shopwarps/commands/ShopsCommand.kt
+++ src/main/kotlin/de/lartyhd/spigot/classic/shopwarps/commands/ShopsCommand.kt
@@ -7,6 +7,7 @@ package de.lartyhd.spigot.classic.shopwarps.commands
 import de.lartyhd.spigot.classic.shopwarps.inventory.WarpsInventory
 import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
 
 /**
  * @author Lars Artmann | LartyHD
@@ -16,11 +17,22 @@ import org.bukkit.plugin.java.JavaPlugin
 class ShopsCommand(javaPlugin: JavaPlugin) : Command(
         javaPlugin = javaPlugin,
         commandName = "Shops",
-        permission = "shops.use"
+        permission = "shops.use",
+        usage = "[delete <UUID>]",
+        maxLength = 2
 ) {
 
     override fun perform(sender: CommandSender, args: Array<String>) = isPlayer(sender) {
         WarpsInventory.openInventory(it)
+        try {
+            if (it.hasPermission("shops.admin.delete") && args.size == 2) {
+                WarpsInventory.remove(UUID.fromString(args[1]))
+                WarpsInventory.updateWarps()
+                sender.sendMessage("§4Du hast den Shop von ${args[1]}")
+            } else sendUseMessage(sender)
+        } catch (ex: Exception) {
+            sender.sendMessage("§4Es ist ein Fehler aufgetreten")
+        }
     }
 
 }
