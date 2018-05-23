92842857a394b41c60e6cc3e797861d9122bfa6d
LartyHD <LartyHD@minewar.de>
2018-05-23 18:50:20 +0200

    Added Messages Enum

--- src/main/kotlin/de/lartyhd/spigot/classic/shopwarps/commands/ShopsCommand.kt
+++ src/main/kotlin/de/lartyhd/spigot/classic/shopwarps/commands/ShopsCommand.kt
@@ -5,6 +5,7 @@
 package de.lartyhd.spigot.classic.shopwarps.commands
 
 import de.lartyhd.spigot.classic.shopwarps.inventory.WarpsInventory
+import de.lartyhd.spigot.classic.shopwarps.utils.Messages
 import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
@@ -25,15 +26,16 @@ class ShopsCommand(javaPlugin: JavaPlugin) : Command(
     override fun perform(sender: CommandSender, args: Array<String>) = isPlayer(sender) {
         if (args.isEmpty()) WarpsInventory.openInventory(it)
         else try {
-            if (args.size == 2)
+            if (args.size == 2 && args[1].equals("delete", true))
                 hasPermission(sender, "shops.admin.delete") {
                     WarpsInventory.removeAndUpdateWarps(UUID.fromString(args[1]))
-                    sender.sendMessage("§4Du hast den Shop von ${args[1]} gelöscht")
+                    sender.sendMessage("${Messages.PREFIX}§bDu hast alle Shop von §9${args[1]} §bgelöscht")
                 }
             else sendUseMessage(sender)
         } catch (ex: Throwable) {
-            sender.sendMessage("§4Es ist ein Fehler aufgetreten §c(mehr Infos in der Console)§4.")
-            System.err.println("Es ist ein Fehler aufgetreten:")
+            val message = "Es ist ein Fehler beim löschen der Warps von ${args[1]} aufgetreten"
+            sender.sendMessage("§4$message §c(mehr Infos in der Console)§4.")
+            System.err.println("$message:")
             ex.printStackTrace()
         }
     }
---
+++ src/main/kotlin/de/lartyhd/spigot/classic/shopwarps/utils/Messages.kt
@@ -0,0 +1,21 @@
+/*
+ * © Copyright - Lars Artmann aka. LartyHD 2018.
+ */
+
+package de.lartyhd.spigot.classic.shopwarps.utils
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 23.05.2018 18:45.
+ * Last edit 23.05.2018
+ */
+enum class Messages(private val message: String) {
+
+    NAME("Shops"),
+    PREFIX("§8[§b$NAME§8] §f"),
+    UNCOLORED_PREFIX("[$NAME] ");
+
+    override fun toString() = message
+
+
+}
